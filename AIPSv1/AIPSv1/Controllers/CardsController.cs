c94ee72b362d761edfa36d69c7dc1e8aeff2db11
KrisKumm <kristijanilic997@gmail.com>
2019-12-26 19:16:16 +0100

    Faza 2
    MySQL DB, ASP.NET CORE 2

---
+++ AIPSv1/AIPSv1/Controllers/CardsController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AIPSv1;
+
+namespace AIPSv1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CardsController : ControllerBase
+    {
+        private readonly gemsandcrystalsContext _context;
+
+        public CardsController(gemsandcrystalsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Cards
+        [HttpGet]
+        public IEnumerable<Card> GetCard()
+        {
+            return _context.Card;
+        }
+
+        // GET: api/Cards/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCard([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var card = await _context.Card.FindAsync(id);
+
+            if (card == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(card);
+        }
+
+        // PUT: api/Cards/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCard([FromRoute] int id, [FromBody] Card card)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != card.IdCard)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(card).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CardExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Cards
+        [HttpPost]
+        public async Task<IActionResult> PostCard([FromBody] Card card)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Card.Add(card);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCard", new { id = card.IdCard }, card);
+        }
+
+        // DELETE: api/Cards/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCard([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var card = await _context.Card.FindAsync(id);
+            if (card == null)
+            {
+                return NotFound();
+            }
+
+            _context.Card.Remove(card);
+            await _context.SaveChangesAsync();
+
+            return Ok(card);
+        }
+
+        private bool CardExists(int id)
+        {
+            return _context.Card.Any(e => e.IdCard == id);
+        }
+    }
+}
---
+++ AIPSv1/AIPSv1/Startup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using Pomelo.EntityFrameworkCore.MySql.Storage.Internal;
+
+namespace AIPSv1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // other service configurations go here
+            // replace "YourDbContext" with the class name of your DbContext
+            ServerVersion s = new ServerVersion(new Version(8, 0, 18), ServerType.MySql);
+
+            services.AddDbContextPool<gemsandcrystalsContext>(options => options
+                // replace with your connection string
+                .UseMySql("Server=localhost;Database=gemsandcrystals;User=root;Password=root;", mySqlOptions => mySqlOptions
+                    // replace with your Server Version and Type
+                    .ServerVersion(s.Version, s.Type)
+            ));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseMvc();
+
+        }
+    }
+}
